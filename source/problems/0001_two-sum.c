71f264728a397b566d9c1a8da341a89e575afa17
wally <buaaben@163.com>
2022-08-25 22:47:44 +0800

    refactor: 使用夹逼法优化`two sum`解法

--- source/problems/0001_two-sum.c
+++ source/problems/0001_two-sum.c
@@ -1,23 +1,45 @@
 #include <stdlib.h>
 
+typedef struct {
+  int value;
+  int index;
+} Object;
+
+static int compare(const void* l, const void* r) {
+  return ((Object*)l)->value - ((Object*)r)->value;
+}
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
+  Object* objs = (Object*)malloc(sizeof(Object) * numsSize);
+  for (int i = 0; i < numsSize; i++) {
+    objs[i].value = nums[i];
+    objs[i].index = i;
+  }
+  qsort(objs, numsSize, sizeof(Object), compare);
+
   int* ans = (int*)malloc(sizeof(int) * 2);
   *returnSize = 2;
-  for (int i = 0; i < numsSize; i++) {
-    for (int j = i+1; j < numsSize; j++) {
-      if (nums[i] + nums[j] == target) {
-        ans[0] = i;
-        ans[1] = j;
-        break;
-      }
+  int i = 0, j = numsSize - 1;
+  int sum;
+  while (i < j) {
+    sum = objs[i].value + objs[j].value;
+    if (sum < target) {
+      i++;
+    } else if (sum > target) {
+      j--;
+    } else {
+      ans[0] = objs[i].index;
+      ans[1] = objs[j].index;
+      break;
     }
   }
+
   return ans;
 }
 
 /*
   Status: Accepted (57/57)
-  Runtime: 190 ms, faster than 41.05% of C submissions.
-  Memory Usage: 6.4 MB, less than 66.04% of C submissions.
+  Runtime: 17 ms, faster than 92.08% of C submissions.
+  Memory Usage: 6.9 MB, less than 8.75% of C submissions.
  */
