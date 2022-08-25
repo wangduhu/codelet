1c36f4e48732e328bcfbcbb7f9fc5fb72fc85d99
wally <buaaben@163.com>
2022-08-25 20:33:08 +0800

    feat: init project `codelet` with cmake and doctest

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil .
+      ((cmake-ide-project-dir . "~/Project/codelet")
+       (cmake-ide-build-dir . "~/Project/codelet/build")
+       (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug")
+       (helm-make-build-dir . "build")
+       (helm-make-arguments . "-j7")
+       (helm-ctest-dir . "~/Project/codelet/build/")
+       )))
---
+++ source/problems/0000-template.c
@@ -0,0 +1,5 @@
+
+int factorial(int number)
+{
+  return number <= 1 ? number : factorial(number - 1) * number;
+}
